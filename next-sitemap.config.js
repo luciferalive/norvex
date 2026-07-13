e35f6522c1d4df2eaf34e5e4f43f0e13d8198fc8
Mohammed Haziq <haziqskp963@gmail.com>
2026-07-13 18:49:59 +0530

    Add sitemap and improve SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://norvex-tawny.vercel.app/",
+  generateRobotsTxt: true,
+  changefreq: "weekly",
+  priority: 0.7,
+  sitemapSize: 7000,
+};
