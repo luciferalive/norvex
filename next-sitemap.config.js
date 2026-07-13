58308db948c6fe83716ab354a62fb5cde25b1abb
Mohammed Haziq <haziqskp963@gmail.com>
2026-07-13 18:55:51 +0530

    Fix sitemap URL

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://norvex-tawny.vercel.app/",
+  siteUrl: "https://norvex-tawny.vercel.app",
   generateRobotsTxt: true,
+  generateIndexSitemap: false,
   changefreq: "weekly",
   priority: 0.7,
   sitemapSize: 7000,
